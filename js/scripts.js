67e73e79fd7107f0a84e1a976c30899c56bf710a
cristian Bertelegni <cristianbertelegni@gmail.com>
2015-02-12 10:33:36 -0300

    agrego linea roja y unifico el grafico 1 y 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,62 @@
-
+// https://docs.google.com/document/d/1nW5JdLrGwxEvFUgXh5qc6II9Z1Dvg7OBqZ0mHz04Ph4/edit#
 const CSV_URL  = 'data/ipc_2005_2014_coeficiente_confusion.csv',
-	CHART_1 = ["IPC.INDEC", "IPC.San.Luis"],
-	CHART_2 = ['IPC.INDEC.acumulado.anual', 'IPC.San.Luis.acumulado.anual'],
-	CHART_3 = ["coeficiente.confusion", "coeficiente.confusion.acumulado.anual"];
+	CHART_1 = ["IPC.San.Luis", "IPC.INDEC" ],
+	CHART_2 = ["IPC.San.Luis.acumulado.anual", "IPC.INDEC.acumulado.anual"],
+	CHART_3 = ["coeficiente.confusion", "coeficiente.confusion.acumulado.anual"],
+	CHART_4 = ["IPC.San.Luis", "IPC.INDEC", 'IPC.San.Luis.acumulado.anual', 'IPC.INDEC.acumulado.anual'],
+	AXES_Y2_CHART_4 = {
+	        "IPC.INDEC.acumulado.anual": 'y2', // ADD
+	        "IPC.San.Luis.acumulado.anual": 'y2' // ADD
+	      };
+
+
 var g_data;
 
 d3.csv(CSV_URL, function(err, data){
 	"use strict";
-	g_data = add_timestamp_to_data(data);
+	// g_data = add_timestamp_to_data(data);
+	g_data = data;
 	
-	drag_chart(CHART_1)
+
+	drag_chart(CHART_4, AXES_Y2_CHART_4);
 });
 
-function drag_chart(arr_keys){
+function drag_chart(arr_keys, axes_y2){
 
 	var data_chart = map_for_chart(g_data, arr_keys);
 	
-	var chart = c3.generate({
-        bindto: '#chart',
-        data: data_chart,
-        axis: {
+	var axis = {
             x: {
                 type: 'timeseries',
                 tick: {
                     format: '%Y-%m-%d'
                 }
             }
-        },
+        };
+
+
+	if (axes_y2){
+		
+		data_chart.axes = axes_y2;
+
+		axis.y2 = {
+            	show: true
+            }
+	
+	}	
+	
+	var chart = c3.generate({
+        bindto: '#chart',
+        data: data_chart,
+        axis: axis,
+        grid: {
+		  x: {
+		  	show: true,
+		    lines: [
+		      {value: "2006-12-01", text: 'Intervención del Idec', class: "red"}
+		    ]
+		  }
+		},
         subchart: {
             show: true
         },
@@ -58,6 +88,7 @@ function map_for_chart (data, arr_keys) {
 	for (i in arr_keys){ // tomo las columnas pedidas
 		columns.push([arr_keys[i]]);
 	}
+
 	data.forEach(function(d){
 		columns[0].push(d['periodo']);
 		for (i in arr_keys){ // tomo las columnas pedidas
